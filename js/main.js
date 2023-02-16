da2f739460018357c92766481119e94c14ac457d
Kalaivani <kalaivani2000@gmail.com>
2023-02-16 15:53:04 +0530

    update

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ let show=document.getElementById("signup");
 let showinfo=document.getElementById("headersection");
 
 
-    let users=[{
+   /*let users=[{
 		email:'jeeva@gmail.com',
 		first_name:'Jeeva',
 		last_name:'K',
@@ -16,7 +16,8 @@ let showinfo=document.getElementById("headersection");
 		gender:'Male',
 		pass:"123",
 		conPass:"123"
-	}];
+	}];*/
+    let users=[];
    let userdata={};
     addElement.addEventListener("click",function(){
     
@@ -35,77 +36,121 @@ let showinfo=document.getElementById("headersection");
     }
     else{
 		alert("Please enter the valid E-mail id");
-		/* 	show=document.getElementById("signup").style.display="block";
-			showNone=document.getElementById("signin").style.display="none"; */
+		let showIn =document.getElementById("net").style.display="none";
+			/*show=document.getElementById("signup").style.display="block";
+			showNone=document.getElementById("signin").style.display="none";*/
+			
 	           
 	}
 });
     addElement1.addEventListener("click",function(){
 			headerIn=document.getElementById("header").style.display="none";
-			showNone.style.display="none";
-			show.style.display="block";
+			show=document.getElementById("signup").style.display="block";
+			showNone=document.getElementById("signin").style.display="none";
+			
 	});	
-    function validate(){
-        var userEmail=document.getElementById("email");
-        var userPassword=document.getElementById("pwd");
-            if(userEmail.value.trim()=="" ||userPassword.value.trim()=="")
-			{
-                alert("No blank value is allowed");
-				return false;
-            }else{
-			    true;	
-			}
-	}
 
-// Sign Up Page 
+///signup
+let form = document.getElementById('form');
+let username = document.getElementById('username');
+let email = document.getElementById('email');
+let phone = document.getElementById('phone');
+let password = document.getElementById('password');
+let password2 = document.getElementById('password2');
 
-let mainTag=document.getElementById("signupbtn");
-let infoTag=document.getElementById("fname");
-let aboutTag=document.getElementById("lname");
-let teamTag=document.getElementById("email2");
-let sectTag=document.getElementById("phone");
-let contTag=document.getElementById("gender");
-let seletTag=document.getElementById("birthday");
-let logTag=document.getElementById("pwd2");
-let footTag=document.getElementById("conpwd2");
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    validateInputs();
+});
 
-mainTag.addEventListener("click",function(){
-    
-    let fieldName=infoTag.value;
-    let sectName=aboutTag.value;
-	let srcName=teamTag.value;
-	let shiftName=sectTag.value;
-	let errorName=contTag.value;
-	let layoutName=seletTag.value;
-	let prtName= logTag.value;
-	let altName=footTag.value;
-    
-    if(prtName===altName){
-		userdata["usrFirstname"]=fieldName;
-		userdata["usrLastname"]=sectName;
-		userdata["email"]=srcName;
-		userdata["phnNum"]=shiftName;
-		userdata["gender"]=errorName;
-		userdata["dob"]=layoutName;
-		userdata["pass"]=prtName;
-		userdata["conPassword"]=altName;
-		users.push(userdata);
-		console.log(users);
-		show.style.display="none";
-		showNone.style.display="block";
+    let setError = (element, message) => {
+    let inputControl = element.parentElement;
+    let errorDisplay = inputControl.querySelector('.error');
+	/*errorDisplay.innerText= message;*/
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+let setSuccess = element => {
+    let inputControl = element.parentElement;
+    let errorDisplay = inputControl.querySelector('.error');
+	errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+let isValidEmail = email => {
+    /*const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());*/
+}
+let isValidPhone = phone =>{
+	
+}
+
+let validateInputs = () => {
+    let usernameValue = username.value.trim();
+    let emailValue = email.value.trim();
+	let phoneValue = phone.value.trim();
+    let passwordValue = password.value.trim();
+    let password2Value = password2.value.trim();
+
+    if(usernameValue === '') {
+        setError(username, 'Username is required');
+    } else {
+        setSuccess(username);
     }
-    else{
-		alert("confirm password is worng?");
-		footTag.value="";
-	}
+
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+	
+	if(phoneValue === '') {
+        setError(phone, 'Phone is required');
+    }else {
+        setSuccess(phone);
+    }
+	
+
+    if(passwordValue === '') {
+        setError(password, 'Password is required');
+    } else if (passwordValue.length < 5) {
+        setError(password, 'Password must be at least 8 character.')
+    } else {
+        setSuccess(password);
+    }
+
+    if(password2Value === '') {
+        setError(password2, 'Please confirm your password');
+    } else if (password2Value !== passwordValue) {
+        setError(password2, "Passwords doesn't match");
+    } else {
+        setSuccess(password2);
+    }
+	let systemIn=document.getElementById("system");
+systemIn.addEventListener("click",function(){
+	//console.log("hi");
 	let headersectionIn=document.getElementById("headersection").style.display="block";
-	let netElm=document.getElementById("net").style.display="block";
+    let netElm=document.getElementById("net").style.display="block";
+	show=document.getElementById("signup").style.display="none";
 	headerIn=document.getElementById("header").style.display="none";
 	showNone=document.getElementById("signin").style.display="none";
-	
 });
 
 
+};
+/*let systemIn=document.getElementById("system");
+systemIn.addEventListener("click",function(){
+	console.log("hi");
+	let headersectionIn=document.getElementById("headersection").style.display="block";
+    let netElm=document.getElementById("net").style.display="block";
+	show=document.getElementById("signup").style.display="none";
+	headerIn=document.getElementById("header").style.display="none";
+	showNone=document.getElementById("signin").style.display="none";
+});*/
 
 //lindkn page
 let userDataElm=document.getElementById("userdata");
@@ -165,7 +210,7 @@ let Data=
   }
   
  ]
- Data.forEach(function(obj){
+ Data.forEach((obj)=>{
  let boxTag=document.createElement("div");
  let blockItemTag=document.createElement("div");
  let blockItemTag1=document.createElement("div");
@@ -192,5 +237,6 @@ let Data=
   blockItemTag1.append(pctTag);
   boxTag.append(blockItemTag,blockItemTag1,contentTag);
   userDataElm.appendChild(boxTag);
-});
-
+ });
+ 
+ 
